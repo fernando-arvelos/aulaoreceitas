1959c41da2e7976547bb7801ed4e072362e0686d
Guilherme Muniz <guilherme.muniz91@gmail.com>
2023-07-11 16:49:43 -0300

    feat: Create the page files and the routes to them in App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,15 @@ function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
-      <Route path="/meals" component={ Recipes } />
+      <Route exact path="/meals" component={ Recipes } />
+      <Route exact path="/meals/:id" component={ RecipeDetails } />
+      <Route exact path="/drinks" component={ Drinks } />
+      <Route exact path="/drinks/:id" component={ RecipeDetails } />
+      <Route exact path="/meals/:id/in-progress" component={ RecipeInProgress } />
+      <Route exact path="/drinks/:id/in-progress" component={ RecipeInProgress } />
+      <Route exact path="/profile" component={ Profile } />
+      <Route exact path="/done-recipes" component={ DoneRecipes } />
+      <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
     </Switch>
   );
 }
