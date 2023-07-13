d787b01b550667f9f69e9395f7e4c63f7dd813ab
gabrielicristina <gabrielicristina189@gmail.com>
2023-07-13 18:42:39 -0300

    Feat: all screen functionalities ok

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import shareIcon from '../images/shareIcon.svg';
 
 function DoneRecipes() {
   const [doneRecipes, setDoneRecipes] = useState([]);
   const [copyMessage, setCopyMessage] = useState('');
+  const [filterType, setFilterType] = useState('');
 
   useEffect(() => {
     const storedRecipes = localStorage.getItem('doneRecipes');
@@ -14,7 +16,8 @@ function DoneRecipes() {
 
   const copyUrlToClipboard = () => {
     const url = 'http://localhost:3000/meals/52771';
-    navigator.clipboard.writeText(url)
+    navigator.clipboard
+      .writeText(url)
       .then(() => {
         setCopyMessage('Link copied!');
       })
@@ -23,45 +26,69 @@ function DoneRecipes() {
       });
   };
 
+  const handleFilter = (type) => {
+    setFilterType(type);
+  };
+
+  const handleClearFilter = () => {
+    setFilterType('');
+  };
+
+  const filteredRecipes = filterType
+    ? doneRecipes.filter((recipe) => recipe.type === filterType)
+    : doneRecipes;
+
   return (
     <div>
-      <button data-testid="filter-by-all-btn">All</button>
-      <button data-testid="filter-by-meal-btn">Meals</button>
-      <button data-testid="filter-by-drink-btn">Drinks</button>
+      <button data-testid="filter-by-all-btn" onClick={ handleClearFilter }>
+        All
+      </button>
+      <button data-testid="filter-by-meal-btn" onClick={ () => handleFilter('meal') }>
+        Meals
+      </button>
+      <button data-testid="filter-by-drink-btn" onClick={ () => handleFilter('drink') }>
+        Drinks
+      </button>
 
       <div id="done-recipes">
-        {doneRecipes.map((recipe, index) => (
+        {filteredRecipes.map((recipe, index) => (
           <div className="recipe-card" key={ index }>
-            <img
-              src={ recipe.image }
-              alt={ recipe.name }
-              data-testid={ `${index}-horizontal-image` }
-            />
+            <Link to={ `/meals/${recipe.id}` }>
+              <img
+                src={ recipe.image }
+                alt={ recipe.name }
+                data-testid={ `${index}-horizontal-image` }
+              />
+            </Link>
             {recipe.type === 'meal' ? (
-              <p data-testid={ `${index}-horizontal-top-text` }>
+              <p
+                data-testid={ `${index}-horizontal-top-text` }
+              >
                 {`${recipe.nationality} - ${recipe.category}`}
+
               </p>
             ) : (
-              <p data-testid={ `${index}-horizontal-top-text` }>
+              <p
+                data-testid={ `${index}-horizontal-top-text` }
+              >
                 {recipe.alcoholic ? 'Alcoholic' : 'Non-Alcoholic'}
+
               </p>
             )}
-            <h3 data-testid={ `${index}-horizontal-name` }>{recipe.name}</h3>
+            <Link to={ `/drinks/${recipe.id}` }>
+              <h3 data-testid={ `${index}-horizontal-name` }>{recipe.name}</h3>
+            </Link>
             <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
             <button
               src={ shareIcon }
-              alt="Share"
               data-testid={ `${index}-horizontal-share-btn` }
-              onClick={ () => copyUrlToClipboard() }
+              onClick={ copyUrlToClipboard }
             >
-              <img src={ shareIcon } alt="Share" />
+              <img src={ shareIcon } alt="Compartilhar" />
             </button>
 
             {recipe.tags.slice(0, 2).map((tag, tagIndex) => (
-              <span
-                key={ tagIndex }
-                data-testid={ `${index}-${tag}-horizontal-tag` }
-              >
+              <span key={ tagIndex } data-testid={ `${index}-${tag}-horizontal-tag` }>
                 {tag}
               </span>
             ))}
