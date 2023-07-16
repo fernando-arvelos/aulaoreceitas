efff6cfb4da1fd7efff12e7fed8fa71a2a86122e
gabrielicristina <gabrielicristina189@gmail.com>
2023-07-16 20:19:55 -0300

    Feat: implemented elements

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function FavoriteRecipes() {
+  const [favoriteRecipes, setFavoriteRecipes] = useState([]);
+
+  useEffect(() => {
+    const favoriteRecipesData = localStorage.getItem('favoriteRecipes');
+    if (favoriteRecipesData) {
+      setFavoriteRecipes(JSON.parse(favoriteRecipesData));
+    }
+  }, []);
+
   return (
     <div>
-      FavoriteRecipes
+      <h1>Favorite Recipes</h1>
+
+      <button data-testid="filter-by-all-btn">All</button>
+      <button data-testid="filter-by-meal-btn">Meals</button>
+      <button data-testid="filter-by-drink-btn">Drinks</button>
+
+      {favoriteRecipes.map((recipe, index) => (
+        <div key={ index }>
+          <img
+            src={ recipe.image }
+            alt={ `${recipe.name} recipe` }
+            data-testid={ `${index}-horizontal-image` }
+          />
+          <p data-testid={ `${index}-horizontal-top-text` }>{recipe.category}</p>
+          <h3 data-testid={ `${index}-horizontal-name` }>{recipe.name}</h3>
+          <button data-testid={ `${index}-horizontal-share-btn` }>Share</button>
+          <button data-testid={ `${index}-horizontal-favorite-btn` }>Favorite</button>
+        </div>
+      ))}
     </div>
   );
 }
