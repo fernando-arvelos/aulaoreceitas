e0a1e1dbf34a31c59ba3ff82de14e8043856845b
Fernando Arvelos <fernandoarvelos1@gmail.com>
2023-07-19 16:28:44 -0300

    feat: Styling the login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,5 +12,12 @@ export const theme = {
     md: '768px',
     lg: '1024px',
   },
+  colors: {
+    red: '#C1292E',
+    yellow: '#F1D302',
+    gray: '#262730',
+    black: '#000000',
+    white: '#FFFFFF',
+  },
 };
 export const plugins = [];
